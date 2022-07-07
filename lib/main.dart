7a43a42c37dd0b9257fcc96c97b7287ce768cfcb
Glebosik <kalandinscoffer@gmail.com>
2022-07-07 20:48:24 +0300

    Prepare to create notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:littlenotes/constatnts/routes.dart';
 import 'package:littlenotes/views/login_view.dart';
-import 'package:littlenotes/views/notes_view.dart';
+import 'package:littlenotes/views/notes/new_note_view.dart';
+import 'package:littlenotes/views/notes/notes_view.dart';
 import 'package:littlenotes/views/register_view.dart';
 import 'package:littlenotes/views/home_page.dart';
 
@@ -17,6 +18,7 @@ void main() {
       loginRoute: (context) => const LoginView(),
       registerRoute: (context) => const RegisterView(),
       notesRoute: ((context) => const NotesView()),
+      newNoteRoute: ((context) => const NewNoteView()),
     },
   ));
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:littlenotes/services/auth/auth_service.dart';
 import 'package:littlenotes/views/login_view.dart';
-import 'package:littlenotes/views/notes_view.dart';
+import 'package:littlenotes/views/notes/notes_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
