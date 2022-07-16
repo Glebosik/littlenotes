a9b4283957b37be0c2812624ad0955bdd6a88383
Glebosik <kalandinscoffer@gmail.com>
2022-07-16 14:15:22 +0300

    Add note sharing functionality

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:littlenotes/services/auth/auth_service.dart';
+import 'package:littlenotes/utilities/dialogs/cannot_share_empty_dialog.dart';
 import 'package:littlenotes/utilities/generics/get_arguments.dart';
 import 'package:littlenotes/services/cloud/cloud_note.dart';
-import 'package:littlenotes/services/cloud/cloud_storage_exceptions.dart';
 import 'package:littlenotes/services/cloud/firebase_cloud_storage.dart';
+import 'package:share_plus/share_plus.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({Key? key}) : super(key: key);
@@ -89,7 +90,22 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('New Note')),
+      appBar: AppBar(
+        title: const Text('New Note'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if (_note == null || text.isEmpty) {
+                await showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          )
+        ],
+      ),
       body: FutureBuilder(
         future: createOrGetNote(context),
         builder: (context, snapshot) {
